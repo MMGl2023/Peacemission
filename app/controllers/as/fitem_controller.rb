c9f59924748d438f50dabf52233ee18c9d44a727
Sergey Petrov <sergeypal1983@mail.ru>
2023-03-02 17:15:04 +0300

    #fix

--- app/controllers/as/fitem_controller.rb
+++ app/controllers/as/fitem_controller.rb
@@ -3,7 +3,7 @@ class As::FitemController < ApplicationController
 
   require_permission :fitems
 
-  active_scaffold :file_item do |config|
+  active_scaffold :fitem do |config|
     config.columns << :url
     # config.columns = [:fhash, filename, :content_type]
     # config.show.columns << [:created_at, :user, :fitems]
