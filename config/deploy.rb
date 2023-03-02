f28c1e44919fe3b9dd74dd70c190937dc5efee05
Sergey Petrov <sergeypal1983@mail.ru>
2023-03-02 18:39:13 +0300

    #try

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,73 +1,27 @@
 # frozen_string_literal: true
-#
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.17.2"
 
-set :application, "rozysk"
+# Change the YOUR_GITHUB_NAME to your github user name
 set :repo_url, "git@github.com:MMGl2023/Peacemission.git"
+set :application, "rozysk"
 set :user, 'webmaster'
-set :puma_threads, [4, 16]
-set :puma_workers, 0
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
 
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+# Don't change these unless you know what you're doing
+set :pty,             true
+set :use_sudo,        false
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log, "#{release_path}/log/puma.access.log"
-
-set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :branch, 'main'
-
-# Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-append :linked_files, "config/database.yml", 'config/master.key', 'config/puma.rb'
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage", "public/assets", "public/s"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-set :keep_releases, 3
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
-
-# Skip migration if files in db/migrate were not modified
-# Defaults to false
-set :conditionally_migrate, true
-
-# Set unique identifier for background jobs
-set :whenever_identifier, -> { "#{fetch(:application)}_#{fetch(:stage)}" }
-
-# ================================================
-# ============ From Custom Rake Tasks ============
-# ================================================
-# ===== See Inside: lib/capistrano/tasks =========
-# ================================================
-
-# set this to false after deploying for the first time
-set :initial, true
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
@@ -82,41 +36,30 @@ namespace :puma do
 end
 
 namespace :deploy do
-  # desc "Make sure local git is in sync with remote."
-  # task :check_revision do
-  #   on roles(:app) do
-  #     unless `git rev-parse HEAD` == `git rev-parse origin/master`
-  #       puts "WARNING: HEAD is not the same as origin/master"
-  #       puts "Run `git push` to sync changes."
-  #       exit
-  #     end
-  #   end
-  # end
-
-  desc 'Initial Deploy'
-  task :initial do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
     on roles(:app) do
-      before 'deploy:migrate', 'database:create'
-      before 'deploy:restart', 'puma:start'
-      invoke 'deploy'
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
+      end
     end
   end
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
+  task :symlink_secrets do
+    on roles(:app) do
+      execute "rm -rf #{release_path}/config/secrets.yml"
+      execute "ln -nfs ~/secrets.yml #{release_path}/config/secrets.yml"
     end
   end
 
-  # upload configuration files
-  before :starting, 'config_files:upload'
-  # before :starting, :check_revision
-  after :finishing, :compile_assets
-  after :finishing, :cleanup
-  after :finishing, :restart
-  # update cron job from whenever schedule file at "config/schedule.rb"
-  after :finishing, 'whenever:update_crontab'
-  # reload application after successful deploy
-  after :publishing, 'application:reload'
+  before :starting,     :check_revision
+  after  :finishing,    :symlink_secrets
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
 end
+
+# ps aux | grep puma    # Get puma pid
+# kill -s SIGUSR2 pid   # Restart puma
+# kill -s SIGTERM pid   # Stop puma
